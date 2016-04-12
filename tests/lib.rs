4fab52ca3291f6414c68e9d620862be7760a136b
Ted Blackman <ted.blackman@gmail.com>
2016-04-12 00:59:03 -0700

    Some file organization.

---
+++ tests/lib.rs
@@ -0,0 +1,23 @@
+extern crate xact;
+
+#[test]
+fn send_small_string() {
+  match xact::send_binary_blob("tcp://127.0.0.1:1234", "msg-0", "ermahgerd".as_bytes(), Duration::from_millis(2000), false, |s| { info!("{}", s) }) {
+    Ok(result_bytes) => { info!("Result: {:?}", result_bytes); },
+    Err(e) => {
+      error!("Error: {}", e.description());
+      panic!(e)
+    }
+  };
+}
+
+#[test]
+fn send_big_vec() {
+  match xact::send_binary_blob("tcp://127.0.0.1:1234", "msg-1", vec![0x2a as u8; 1e8 as usize].as_slice(), Duration::from_millis(20000), false, |s| { info!("{}", s) }) {
+    Ok(result_bytes) => { info!("Result: {:?}", result_bytes); },
+    Err(e) => {
+      error!("Error: {}", e.description());
+      panic!(e)
+    }
+  };
+}
